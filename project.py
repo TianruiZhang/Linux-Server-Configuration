153a3e85aeedf48012849257e97d9f1c926a7bb8
vagrant <vagrant@vagrant.vm>
2018-10-05 13:06:36 +0000

    homepage datasetup done

--- project.py
+++ project.py
@@ -15,14 +15,22 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route("/")
-@app.route("/selections/<int:selection_id>/")
-def selectionMenu(selection_id):
+@app.route("/home/")
+def homepage():
     selections = session.query(Selection).all()
-    items = session.query(MenuItem).\
+    items = (session.query(MenuItem.name, Selection.name)).\
+        join(Selection, MenuItem.selection_id == Selection.id).\
         order_by(MenuItem.time_created.desc()).limit(5).all()
     return render_template("home.html", selections=selections, items=items)
+
+#@app.route("/selections/<int:selection_id>/")
+#def selectionMenu(selection_id):
+    #selection = session.query(Selection).filter_by(id=selection_id).one()
+    #items = session.query(MenuItem).filter_by(selection_id=selection.id)
+    #return render_template("menu.html", selection=selection, items=items)
+
 
 if __name__ == "__main__":
     app.secret_key = "super_secret_key"
     app.debug = True
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
