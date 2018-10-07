3337f437e25cf126c91e367884c3929b01236b88
vagrant <vagrant@vagrant.vm>
2018-10-07 09:25:33 +0000

    Read front-end finished

--- project.py
+++ project.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request,\
-    redirect, url_for, flash, jsonify
-from sqlalchemy import create_engine
+from flask import Flask, render_template, request, redirect,\
+                    url_for, flash, jsonify
+from sqlalchemy import create_engine, and_
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Selection, MenuItem
 
@@ -23,12 +23,20 @@ def homepage():
         order_by(MenuItem.time_created.desc()).limit(5).all()
     return render_template("home.html", selections=selections, items=items)
 
-#@app.route("/selections/<int:selection_id>/")
-#def selectionMenu(selection_id):
-    #selection = session.query(Selection).filter_by(id=selection_id).one()
-    #items = session.query(MenuItem).filter_by(selection_id=selection.id)
-    #return render_template("menu.html", selection=selection, items=items)
+@app.route("/selections/<int:selection_id>/")
+def selectionMenu(selection_id):
+    selections = session.query(Selection).all()
+    selection = session.query(Selection).filter_by(id=selection_id).one()
+    items = session.query(MenuItem).filter_by(selection_id=selection.id)
+    return render_template("selectionMenu.html", selections=selections,\
+                            selection=selection, items=items)
 
+@app.route("/selections/<int:selection_id>/<int:menu_id>/")
+def menuDetails(selection_id, menu_id):
+    item = session.query(MenuItem).\
+        filter(and_(MenuItem.selection_id == selection_id, \
+        MenuItem.id == menu_id)).one()
+    return render_template("details.html", item=item)
 
 if __name__ == "__main__":
     app.secret_key = "super_secret_key"
